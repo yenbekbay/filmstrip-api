950546fdd7baa8bb01ae13a366919e811b8ce515
Ayan Yenbekbay <ayan.yenb@gmail.com>
2016-11-25 16:31:20 +0600

    revert: "feat(server): remove cors as it provides no real value"

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,7 @@
 import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
 import { printSchema } from 'graphql/utilities/schemaPrinter';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
 import type { $Request, $Response } from 'express';
@@ -12,6 +13,7 @@ import Logger from '../Logger';
 import schema from './schema';
 
 const GRAPHQL_PORT = 8080;
+const CORS_PORT = 3000;
 
 const graphQLLogger = new Logger('graphql-server');
 const graphQLServer = express();
@@ -24,18 +26,26 @@ graphQLServer.use(morgan(
 graphQLServer.use(bodyParser.urlencoded({ extended: true }));
 graphQLServer.use(bodyParser.json());
 
-graphQLServer.use('/graphql', graphqlExpress((req: $Request) => {
-  // Get the query, the same way express-graphql does it
-  // https://git.io/vXO1c
-  const query = req.query.query || (req.body: any).query;
-  if (query && query.length > 2000) {
-    // None of our app's queries are this long
-    // Probably indicates someone trying to send an overly expensive query
-    throw new Error('Query too large');
-  }
+graphQLServer.use('/graphql',
+  cors({
+    origin: isProduction
+      ? 'https://filmstrip.cf'
+      : `http://localhost:${CORS_PORT}`,
+    methods: ['POST'],
+  }),
+  graphqlExpress((req: $Request) => {
+    // Get the query, the same way express-graphql does it
+    // https://git.io/vXO1c
+    const query = req.query.query || (req.body: any).query;
+    if (query && query.length > 2000) {
+      // None of our app's queries are this long
+      // Probably indicates someone trying to send an overly expensive query
+      throw new Error('Query too large');
+    }
 
-  return { schema };
-}));
+    return { schema };
+  }),
+);
 
 if (!isProduction) {
   graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
