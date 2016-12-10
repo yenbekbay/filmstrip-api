efcf506fd54997acfd06e62da789b63e9ddf46fa
Ayan Yenbekbay <ayan.yenb@gmail.com>
2016-12-10 08:10:03 +0600

    refactor: remove broken migration

--- src/migrations/index.js
+++ src/migrations/index.js
@@ -1,6 +1,3 @@
 /* @flow */
 
-import convertMoviesToMultiLang from './convertMoviesToMultiLang';
-
-// eslint-disable-next-line import/prefer-default-export
-export { convertMoviesToMultiLang };
+export {};
