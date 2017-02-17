67a34246f417e5650ac02b0175ae4a51551f3507
Ayan Yenbekbay <ayan.yenb@gmail.com>
2017-02-18 01:24:45 +0600

    chore(eslint): update eslint-config-anvilabs to latest version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
-  extends: ['anvilabs', 'anvilabs/jest', 'anvilabs/lodash'],
+  extends: [
+    'anvilabs',
+    'anvilabs/flowtype',
+    'anvilabs/jest',
+    'anvilabs/lodash',
+  ],
   rules: {
     'no-magic-numbers': 0,
   },
